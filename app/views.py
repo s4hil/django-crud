2bdd3672995ee48db0a742ed9877af8aecd13211
s4hil <s4hilp4rr4y@gmail.com>
2024-05-02 21:22:39 +0530

    all fts

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import users
 
 # Create your views here.
@@ -22,3 +22,25 @@ def insertData(request):
         query.save()
     return render(request, "index.html", context)
     
+def updateData(request, id):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+
+        e = users.objects.get(id=id)
+        e.name = name
+        e.email = email
+        e.phone = phone
+        e.save()
+        return redirect("/")
+    data = users.objects.get(id=id)
+    context = {"d": data}
+    
+    return render(request, "update.html", context)
+    
+
+def deleteData(request, id):
+        query = users.objects.get(id=id)
+        query.delete()
+        return redirect("/")
